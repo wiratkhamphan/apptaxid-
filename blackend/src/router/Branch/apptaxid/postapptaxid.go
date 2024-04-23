9e1b290d562fee61754ce59f66eeddd408727a09
Rlek <wirat0khamphan@gmail.com>
2024-04-23 17:58:46 +0700

    get post and dbdatae

--- blackend/src/router/Branch/apptaxid/postapptaxid.go
+++ blackend/src/router/Branch/apptaxid/postapptaxid.go
@@ -11,9 +11,9 @@ func Postapptaxid(c *fiber.Ctx) error {
 	c.Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	c.Set("Access-Control-Allow-Headers", "Content-Type")
 	// Parse request body into a new Person struct
-	Tax := new(Taxid)
-	if err := c.BodyParser(Tax); err != nil {
-		return err
+	var Tax Taxid
+	if err := c.BodyParser(&Tax); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"Error": "Failed to parse request body"})
 	}
 
 	// Get a database connection
@@ -22,14 +22,12 @@ func Postapptaxid(c *fiber.Ctx) error {
 		return err
 	}
 	defer db.Close()
-
 	// Execute SQL insert statement
 	_, err = db.Exec("INSERT INTO apptaxid (request_no, date_time, name_customer, status, recorder, inspector, attached_documents, note) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
 		Tax.Request_no, Tax.Date_time, Tax.Name_customer, Tax.Status, Tax.Recorder, Tax.Inspector, Tax.Attached_documents, Tax.Note)
 	if err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to insert new apptaxid entry"})
 	}
-
 	// Return success response
-	return c.SendString("Successfully inserted new apptaxid entry")
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "Successfully inserted new apptaxid entry"})
 }
